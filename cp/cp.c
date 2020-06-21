dd8e6f5ce9a41f31688ad5957f1b7f48e195bbe6
William Gherman <williamgherman@gmail.com>
2020-06-21 13:13:23 +0100

    forbid copying to self, Makefile fixes

--- cp/cp.c
+++ cp/cp.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 int main(int argc, char **argv)
 {
@@ -8,6 +9,12 @@ int main(int argc, char **argv)
 
     if (argc != 3) {
         fprintf(stderr, "usage: %s source_file target_file\n", argv[0]);
+        exit(EXIT_FAILURE);
+    }
+
+    if (strcmp(argv[1], argv[2]) == 0) {
+        fprintf(stderr, "Error: source file and target file must not be the "
+                        "same.\n");
         exit(EXIT_FAILURE);
     }
 
